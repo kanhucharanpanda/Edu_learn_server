dead639f56be60f8ed5165ee56ae9dfd8802ac87
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-07-23 00:27:53 +0530

    Update newsletter.js

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,26 +1,3 @@
-// import express from "express";
-// import {
-//   subscribeNewsletter,
-//   unsubscribeNewsletter,
-//   getNewsletterStats,
-//   getAllSubscribers,
-//   updatePreferences,
-// } from "../controllers/newsletter.js";
-// import { isAuth } from "../middlewares/isAuth.js";
-// import { isAdmin } from "../middlewares/isAdmin.js";
-
-// const router = express.Router();
-
-// // Public routes
-// router.post("/newsletter/subscribe", subscribeNewsletter);
-// router.get("/newsletter/unsubscribe", unsubscribeNewsletter);
-// router.put("/newsletter/preferences", updatePreferences);
-
-// // Admin routes
-// router.get("/newsletter/stats", isAuth, isAdmin, getNewsletterStats);
-// router.get("/newsletter/subscribers", isAuth, isAdmin, getAllSubscribers);
-
-// export default router;
 
 import express from "express";
 import {
