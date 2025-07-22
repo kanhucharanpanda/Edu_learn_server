b6a289ab91627b8ec82c685aec24354e3dfc5916
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-07-23 00:27:31 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -1,26 +1,3 @@
-// import express from "express";
-// import {
-//   getAllCourses,
-//   getSingleCourse,
-//   fetchLectures,
-//   fetchLecture,
-//   getMyCourses,
-//   checkout,
-//   paymentVerification,
-// } from "../controllers/course.js";
-// import { isAuth } from "../middlewares/isAuth.js";
-
-// const router = express.Router();
-
-// router.get("/course/all", getAllCourses);
-// router.get("/course/:id", getSingleCourse);
-// router.get("/lectures/:id", isAuth, fetchLectures); //To get all lectures for a course:
-// router.get("/lecture/:id", isAuth, fetchLecture); //To get a single lecture
-// router.get("/mycourse", isAuth, getMyCourses);
-// router.post("/course/checkout/:id", isAuth, checkout);
-// router.post("/verification/:id", isAuth, paymentVerification);
-
-// export default router;
 
 import express from "express";
 import {
