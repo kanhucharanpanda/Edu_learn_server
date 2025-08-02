6cb47e08dfc5e6027b2f7a031f4807b83c0efb9f
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-08-02 12:03:29 +0530

    Update TryCatch.js

--- middlewares/TryCatch.js
+++ middlewares/TryCatch.js
@@ -1,8 +1,27 @@
+// const TryCatch = (handler) => {
+//   return async (req, res, next) => {
+//     try {
+//       await handler(req, res, next);
+//     } catch (error) {
+//       res.status(500).json({
+//         message: error.message,
+//       });
+//     }
+//   };
+// };
+
+// export default TryCatch;
+
+
+
 const TryCatch = (handler) => {
   return async (req, res, next) => {
     try {
       await handler(req, res, next);
     } catch (error) {
+      // THIS IS THE LINE YOU MUST ADD
+      console.error("AN ERROR OCCURRED:", error);
+
       res.status(500).json({
         message: error.message,
       });
