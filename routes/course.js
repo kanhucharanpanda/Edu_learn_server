af78d6088e1b434c987ec89dd973edbb7742065a
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-08-07 02:04:42 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -8,38 +8,24 @@ import {
   getMyCourses,
   checkout,
   paymentVerification,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-  getAdminCourses,
-} from "../controllers/course.js";
-import { isAuth } from "../middlewares/isAuth.js";
-import { isAdmin } from "../middlewares/isAdmin.js";
-import {
   checkCourseCompletion,
   generateCertificateData,
 } from "../controllers/course.js";
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
-// Public routes
+// Public routes for anyone
 router.get("/course/all", getAllCourses);
 router.get("/course/:id", getSingleCourse);
 
-// User routes
+// Routes for logged-in users
 router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
 router.post("/course/checkout/:id", isAuth, checkout);
 router.post("/verification/:id", isAuth, paymentVerification);
-
-// Admin routes
-router.get("/admin/courses", isAuth, isAdmin, getAdminCourses);
-router.post("/admin/course", isAuth, isAdmin, createCourse);
-router.put("/admin/course/:id", isAuth, isAdmin, updateCourse);
-router.delete("/admin/course/:id", isAuth, isAdmin, deleteCourse);
 router.get("/course/completion/:courseId", isAuth, checkCourseCompletion);
-// Route to generate certificate data (assuming completion verified)
 router.get("/course/:courseId/certificate", isAuth, generateCertificateData);
 
 export default router;
