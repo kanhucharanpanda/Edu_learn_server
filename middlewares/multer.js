e7eaf26a029183eb913da66b11d59c45876d7961
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-08-07 01:49:36 +0530

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,19 +1,35 @@
+// import multer from "multer";
+// import { v4 as uuid } from "uuid";
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "uploads");
+//   },
+//   filename(req, file, cb) {
+//     const id = uuid();
+
+//     const extName = file.originalname.split(".").pop();
+
+//     const fileName = `${id}.${extName}`;
+
+//     cb(null, fileName);
+//   },
+// });
+
+// export const uploadFiles = multer({ storage }).single("file");
+
+
 import multer from "multer";
-import { v4 as uuid } from "uuid";
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads");
+  destination: function (req, file, cb) {
+    // This is a temporary folder to store the file before uploading to Cloudinary
+    cb(null, "./public/temp");
   },
-  filename(req, file, cb) {
-    const id = uuid();
-
-    const extName = file.originalname.split(".").pop();
-
-    const fileName = `${id}.${extName}`;
-
-    cb(null, fileName);
+  filename: function (req, file, cb) {
+    // Use the original filename for the temporary file
+    cb(null, file.originalname);
   },
 });
 
-export const uploadFiles = multer({ storage }).single("file");
+export const uploadFiles = multer({ storage });
